4fa77c50682573211d97f95aa2cdc10252a472ea
Dominik Ujházy <dominik.ujhazy@cleverlance.com>
2023-06-15 18:26:05 +0200

    fix: adapted fetch to pagination

--- src/hooks/useProject.ts
+++ src/hooks/useProject.ts
@@ -1,10 +1,10 @@
-import { mockedMyProjects } from "@/mocks/myProjectsMock";
-import { MyProject } from "@/models/Project";
-import { useQuery } from "@tanstack/react-query";
+import { mockedMyProjects } from '@/mocks/myProjectsMock'
+import { MyProject } from '@/models/Project'
+import { useQuery } from '@tanstack/react-query'
 
-export const getMyProjectsKey = () => ["myProject"];
+export const getMyProjectsKey = () => ['myProject']
 
 export const useProject = (projectId: string) =>
   useQuery<MyProject | undefined>(getMyProjectsKey(), () =>
-    mockedMyProjects.find((project) => project.id === projectId)
-  );
+    mockedMyProjects.docs.find((project) => project.id === projectId)
+  )
