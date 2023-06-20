fbd65fc9a0d2a4680487e89f20f228332e6a6745
Stanislav Zeman <zeman@standa.dev>
2023-06-20 19:21:35 +0200

    feat(api): add /projects/:id/members request validation

--- src/app/api/projects/[projectId]/members/route.ts
+++ src/app/api/projects/[projectId]/members/route.ts
@@ -1,20 +1,30 @@
 import { NextRequest } from 'next/server'
-import { unauthorizedResponse, parseResult } from '@/app/api/helpers'
+import {unauthorizedResponse, parseResult, badRequestResponse} from '@/app/api/helpers'
 import { getAuth } from '@clerk/nextjs/server'
-import { ProjectMemberData, ProjectParams } from '@/app/api/types'
+import { ProjectParams } from '@/app/api/types'
 import { createProjectMember } from '@/repositories/user/create'
-import { Role } from '@prisma/client'
+import {z} from "zod";
+import validation from "@/app/api/validation";
 
-export async function POST(
-  request: NextRequest,
-  context: { params: ProjectParams }
-): Promise<Response> {
+const postValidator = z.object({
+  memberId: z.string(),
+  role: validation.role,
+}).strict()
+
+export async function POST(request: NextRequest, context: { params: ProjectParams }): Promise<Response> {
   const userAuth = getAuth(request)
   if (userAuth.userId === null) {
     return unauthorizedResponse()
   }
-  
-  const data: ProjectMemberData = JSON.parse(await request.text())
+
+  const payload = await request.json();
+  const validationResult = postValidator.safeParse(payload);
+
+  if (!validationResult.success) {
+    return badRequestResponse();
+  }
+
+  const data = payload.data;
   const result = await createProjectMember({
     userId: userAuth.userId,
     projectId: context.params.projectId,
