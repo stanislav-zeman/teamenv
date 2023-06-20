83c7f1e2f157b0282c471eb589223c4c83aef88d
Martin Juhas <511735@mail.muni.cz>
2023-06-20 18:11:25 +0200

    feat(api): add create endpoint for projects

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,9 +1,11 @@
 import {NextRequest} from "next/server";
 import read from "../../../repositories/project/read";
+import projects from "@/repositories/project";
 import {getAuth} from "@clerk/nextjs/server";
 import {ReadonlyURLSearchParams} from "next/navigation";
 import {parseResult, unauthorizedResponse} from "@/app/api/helpers";
 import {parseFiltersFromParams} from "@/models/Filters";
+import {ProjectCreateData} from "@/app/api/types";
 
 export async function GET(request: NextRequest) : Promise<Response> {
   const user = getAuth(request);
@@ -15,7 +17,18 @@ export async function GET(request: NextRequest) : Promise<Response> {
   const readonlySearchParams = new ReadonlyURLSearchParams(searchParams);
   const filters = { userId: user.userId, ...parseFiltersFromParams(readonlySearchParams) }
 
-  const result = await read.all(filters);
+  const result = await projects.read.all(filters);
 
   return parseResult(result, 200);
+}
+
+export async function POST(request: NextRequest) : Promise<Response> {
+  const user = getAuth(request);
+  if (!user.userId) {
+    return unauthorizedResponse();
+  }
+
+  const data: ProjectCreateData = JSON.parse(await request.json());
+  const result = await projects.create({userId: user.userId, ...data});
+  return parseResult(result, 201);
 }
