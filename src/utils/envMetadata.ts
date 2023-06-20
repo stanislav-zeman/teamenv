24567bd72bcbee6d13109a614546d2b1c7aa7a17
Adam Zitnik <xzitnik@fi.muni.cz>
2023-06-20 19:17:01 +0200

    feat: lol

--- src/utils/envMetadata.ts
+++ src/utils/envMetadata.ts
@@ -1,5 +1,5 @@
-const DATABASE_URL = process.env.DATABASE_URL;
-const HOST = process.env.HOST;
+const DATABASE_URL = process.env.DATABASE_URL ?? "postgres://EnsIaver:Sz6gQmCK9Dcq@ep-noisy-pine-411524-pooler.eu-central-1.aws.neon.tech/neondb?pgbouncer=true&connect_timeout=10";
+const HOST = process.env.HOST ?? "http://localhost:3000";
 
 if (typeof window !== "undefined") {
   if (!DATABASE_URL) throw new Error("DATABASE_URL is not defined");
